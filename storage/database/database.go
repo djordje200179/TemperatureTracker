f52773418c3a72c06855bf6f760c7b22f90348c2
Đorđe Milanović <djordje200179@gmail.com>
2022-11-01 18:05:47 +0100

    Database connection implemented

--- storage/database/database.go
+++ storage/database/database.go
@@ -2,10 +2,21 @@ package database
 
 import (
 	"TemperatureTracker/sensor"
+	"TemperatureTracker/storage"
 	"database/sql"
+	_ "modernc.org/sqlite"
 )
 
-type database sql.Conn
+type database sql.DB
+
+func Open(path string) (storage.Storage, error) {
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		return nil, err
+	}
+
+	return (*database)(db), nil
+}
 
 func (storage *database) AddReading(reading *sensor.Reading) error {
 	//TODO implement me
